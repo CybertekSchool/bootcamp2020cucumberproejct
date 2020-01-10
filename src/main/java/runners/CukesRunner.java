a67f4002f14884019d314aad048d8f4e7b405a3d
github@cybertekschool.com <github@cybertekschool.com>
2020-01-10 12:36:25 -0500

    runner class

--- src/main/java/runners/CukesRunner.java
+++ src/main/java/runners/CukesRunner.java
@@ -1,4 +1,13 @@
 package runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "step_definitions",
+        features = "src/main/resources/features"
+)
 public class CukesRunner {
 }
