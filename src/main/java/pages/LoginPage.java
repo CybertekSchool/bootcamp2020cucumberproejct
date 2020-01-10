bf65f7e7c91391e6db245e5b847cccfd4b95fd7d
github@cybertekschool.com <github@cybertekschool.com>
2020-01-10 12:59:06 -0500

    Added login elements

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -13,6 +14,6 @@ public class LoginPage extends BasePage {
 
     public void login(String username, String password) {
         userNameInputBoxElement.sendKeys(username);
-        passwordInputBoxElement.sendKeys(password);
+        passwordInputBoxElement.sendKeys(password, Keys.ENTER);
     }
 }
--- src/main/java/step_definitions/LoginStepDefinitions.java
+++ src/main/java/step_definitions/LoginStepDefinitions.java
@@ -1,22 +1,29 @@
 package step_definitions;
 
 import io.cucumber.java.en.*;
+import pages.LoginPage;
 import utilities.ConfigurationReader;
 import utilities.Driver;
 
 public class LoginStepDefinitions {
+    //this is how we create page objects
+    LoginPage loginPage = new LoginPage();
 
     @Given("user is on the login page")
     public void user_is_on_the_login_page() throws InterruptedException {
         System.out.println("user is on the login page");
         String url = ConfigurationReader.getProperty("url");
         Driver.getDriver().get(url);//to open login page
-        Thread.sleep(1000);
+        Thread.sleep(2000);
     }
 
     @Then("user logs in")
-    public void user_logs_in() {
+    public void user_logs_in() throws InterruptedException {
         System.out.println("user logs in");
+        String username = ConfigurationReader.getProperty("user_name");
+        String password = ConfigurationReader.getProperty("password");
+        loginPage.login(username, password);
+        Thread.sleep(2000);
     }
 
 }
