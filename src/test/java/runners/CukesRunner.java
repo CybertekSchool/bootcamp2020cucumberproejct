ef53c381b59d83879ca9a2d396b750d383014482
github@cybertekschool.com <github@cybertekschool.com>
2020-01-10 14:57:52 -0500

    added cucumber html reporting

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -9,7 +9,8 @@ import org.junit.runner.RunWith;
         glue = "step_definitions",
         features = "src/main/resources/features",
         tags = "@view_orders",
-        dryRun = false
+        dryRun = false,
+        plugin = "json:target/cucumber1.json"
 )
 public class CukesRunner {
 }
--- src/test/java/runners/CukesRunner2.java
+++ src/test/java/runners/CukesRunner2.java
@@ -1,4 +1,5 @@
 package runners;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
@@ -8,7 +9,9 @@ import org.junit.runner.RunWith;
         glue = "step_definitions",
         features = "src/main/resources/features",
         tags = "@login",
-        dryRun = false
+        dryRun = false,
+        plugin = "json:target/cucumber2.json"
+
 )
 public class CukesRunner2 {
 }
